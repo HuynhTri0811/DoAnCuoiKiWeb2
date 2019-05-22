e1527b4ee1953f4cc790e236c318a7fee2857fea
HuynhTri0811 <huynhhuutri0811@gmail.com>
2019-05-22 13:03:44 +0700

    update home.ejs

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,16 @@
 const Router = require('express').Router;
 const fs = require('fs');
+const Sequelize = require('sequelize');
 const Film = require('../models/Film.js');
 
 const router = new Router();
-router.get('/',function(req,res){
-	const film = Film.max('film_ViewCount').then(max => 
-		{
-
-		});	
+router.get('/',async function(req,res){
+	const film = await Film.findAll({
+		order : [
+			['film_ViewCount' , 'DESC']	
+		]
+	});
+	console.log(film[0].dataValues.film_Name);
 	res.render('home.ejs',{film});
 });
 router.get('/forgotPassword',function(req,res){
