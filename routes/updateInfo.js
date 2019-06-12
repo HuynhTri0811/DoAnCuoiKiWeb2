2dc1ab966cd6d9c9a98e478df86e3af5732a44d7
Kingtrungtruc <1660659@student.hcmus.edu.vn>
2019-06-12 15:47:57 +0700

    user tab final

--- routes/updateInfo.js
+++ routes/updateInfo.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const User = require('../models/User.js');
+var formidable = require('formidable'); 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 const router = new Router();
@@ -15,7 +18,7 @@ router.get('/',async function(req,res){
 	{
 		user = await User.findOne({
 			where :{
-				user_ID : user_Id 
+				user_ID : user_Id, 
 			},
 		});
     }
@@ -27,5 +30,94 @@ router.get('/',async function(req,res){
         res.redirect('/');
     }
 });
+
+router.post('/pagedf',async function(req,res){
+    const { user_Id } = req.session;
+    req.session.user_Id = user_Id;
+	res.redirect('/');
+});
+
+router.post('/name',async function(req,res){
+    const {user_Id} = req.session;
+    var form = new formidable.IncomingForm();
+    var {txtUserName} = req.body;
+    if(form){
+        if(user_Id){
+            await User.update({
+                user_Name : txtUserName ,
+                },
+                { where : {
+                    user_ID : user_Id,
+                    },
+            }).then(function(){
+                res.redirect('/updateInfo');
+            }).catch(function(){
+                res.render('404NotFound.ejs');
+            });
+        };
+    }; 
+});
+router.post('/numberphone',async function(req,res){
+    const {user_Id} = req.session;
+    var form = new formidable.IncomingForm();
+    var {txtUserNumberPhone} = req.body;
+    if(form){
+        if(user_Id){
+            await User.update({
+                user_NumberPhone : txtUserNumberPhone ,
+                },
+                { where : {
+                    user_ID : user_Id,
+                    },
+            }).then(function(){
+                res.redirect('/updateInfo');
+            }).catch(function(){
+                res.render('404NotFound.ejs');
+            });
+        };
+    }; 
+});
+router.post('/email',async function(req,res){
+    const {user_Id} = req.session;
+    var form = new formidable.IncomingForm();
+    var {txtUserEmail} = req.body;
+    if(form){
+        if(user_Id){
+            await User.update({
+                user_Email : txtUserEmail ,
+                },
+                { where : {
+                    user_ID : user_Id,
+                    },
+            }).then(function(){
+                res.redirect('/updateInfo');
+            }).catch(function(){
+                res.render('404NotFound.ejs');
+            });
+        };
+    }; 
+});
+router.post('/password',async function(req,res){
+    const {user_Id} = req.session;
+    var form = new formidable.IncomingForm();
+    var {txtUserPassword} = req.body;
+    if(form){
+        if(user_Id){
+            const hash_pass = await bcrypt.hash(txtUserPassword,saltRounds);
+            await User.update({
+                user_Password : hash_pass ,
+                },
+                { where : {
+                    user_ID : user_Id,
+                    },
+            }).then(function(){
+                res.redirect('/updateInfo');
+            }).catch(function(){
+                res.render('404NotFound.ejs');
+            });
+        };
+    }; 
+});
+
 
 module.exports =router;
